3939a75a192f46d58555b5a9b9e3217b2c609a8c
notapresent <notapresent@gmail.com>
2015-04-03 21:05:20 +0000

    Add adapter for function.mil.ru

---
+++ src/MilCrutch.php
@@ -0,0 +1,41 @@
+<?php
+namespace KimonoCrutch;
+
+class MilCrutch extends BaseCrutch {
+    protected $index_url = 'http://dyn.function.mil.ru/news_page/country.htm';
+    public function __construct(){
+        $this->qpopts = array(
+            'convert_to_encoding' => 'utf-8',
+        );
+    }
+
+    protected function get_links($html) {
+        $result = array();
+
+        $links = htmlqp($html, 'div#center>div.newsitem>a', $this->qpopts);
+        foreach($links as $i) {
+            $result[] = array(
+                'title' => $i->text(),
+                'url' => $i->attr('href')
+            );
+        }
+        return $result;
+    }
+
+    protected function get_detail($html) {
+        $title = htmlqp($html, 'div#content>div#center>h1', $this->qpopts)->text();
+        $fulltext = htmlqp($html,'div#content>div#center', $this->qpopts)->text();
+        $fulltext = mb_substr(
+            $fulltext,
+            mb_strpos($fulltext, $title) + mb_strlen($title)
+        );
+        if(mb_strpos($fulltext, 'Метки:')) {
+            $fulltext = mb_substr($fulltext, 0, mb_strpos($fulltext, 'Метки:'));
+        }
+        return array(
+            'title' => $title,
+            'fulltext' =>  $fulltext
+        );
+    }
+}
+
